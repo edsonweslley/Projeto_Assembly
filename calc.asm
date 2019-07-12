6ff5a85bed844cab5406da4481703670b9adac7c
João Pedro de Barros <joao.pedro.barbosa@ccc.ufcg.edu.br>
2019-07-12 14:36:07 -0300

    Adição de inputs e impressão de resultado
    Todas as operações estão funcionais
    
    Falta a selecao de operacoes
    Tentar melhorar a parte de impressão do resultado, deixando tudo em uma função
    
    Tive que sair, mals :D

--- calc.asm
+++ calc.asm
@@ -1,16 +1,77 @@
+.data
+	inicial:				.asciz "== Ultimate Calculadora==\n (+) Adicao\n (-) Subtracao\n (*) Multiplicacao\n (/) Divisao\n"
+	
+	operador:				.space 1
+	
+	resultadoMensagem: 		.asciz "Resultado da operacao: "
+	
+	adicaoMensagem:			.asciz "adicao: "
+	subtracaoMensagem:		.asciz "subtracao: "
+	multiplicacaoMensagem:	.asciz "multiplicacao: "
+	divisaoMensagem:		.asciz "divisao: "
+	
+	nl: 					.asciz "\n"
+
 .text 
-  
-    li  a7, 5 	#lê um numero inteiro     
-    ecall 	# faz essa chamada para ler o inteiro
+
+_start:
+	li a7, 4
+	la a0, inicial
+	ecall		#Mostra a mensagem de introducao da calculador
+	
+	#Leitura dos operandos begin
+    li a7, 5 	# Define o servico a ser realizado como leitura de inteiros    
+    ecall 		# Realiza a leitura do primeiro operando e o coloca em a0
     
-    mv t0,a0	#move o inteiro que recebeu para t0
+    mv t0,a0	# Move o inteiro recebido para t0
     
-    li a6 , 5	#lê um numero inteiro
-    ecall	#faz a chamada para leer o inteiro
+    li a7, 5	# Define o servico a ser realizado como leitura de inteiros
+    ecall		# Realiza a leitura do segundo operando e o coloca em a0
     
-    mv t1,a0	#move o inteiro que recebeu para t1
+    mv t1,a0	# Move o inteiro recebido para t1
+	#Leitura dps operandos end
+	
+	j adicao	
+	
+	
+adicao: 
+	add s2,t0,t1		# Realiza a operacao de soma
+	la s3, adicaoMensagem
+	j imprimeResultado
+
+subtracao:
+	sub s2,t0,t1		# Realiza a operacao de subtração
+	la s3, subtracaoMensagem
+	j imprimeResultado
     
-    add s2,t0,t1	# realiza a soma
-    sub s3,t0,t1	# realiza a subtração
-  
-       
+multiplicacao:
+	mul s2,t0,t1		# Realiza a operacao de multiplicacao
+	la s3, multiplicacaoMensagem
+	j imprimeResultado
+   	
+divisao:
+	div s2,t0,t1		# Realiza a operacao de divisao
+	la s3, divisaoMensagem
+	j imprimeResultado
+	
+imprimeResultado:
+	#Displays resultadoMensagem
+	li a7, 4
+	la a0, resultadoMensagem
+	ecall
+	
+	
+	li a7, 1			# Define o servico a ser realizado como impressao de inteiros
+	add a0, s2, zero    # Adiciona o resultado da operacao realizada no registrador de saida
+    ecall				# Realiza a escrita do inteiro no terminal
+    
+    
+    #Displays resultadoMensagem
+	li a7, 4
+	la a0, nl
+	ecall
+    j _start			# Retorna para o inicio do programa
+    
+fechar:					#Finaliza o programa
+	li a7, 10
+	ecall
